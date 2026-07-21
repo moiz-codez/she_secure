498b7a834cd96b8845eabd7ae0458ecc7e4cc691
moiz-codez <moiz.codez+github@gmail.com>
2026-07-21 05:07:00 +0500

    feat: configure Firebase for Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/router/app_router.dart';
+import 'firebase_options.dart';
 import 'shared/theme/app_theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: SheSecureApp()));
 }
 
